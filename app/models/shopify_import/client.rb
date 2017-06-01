0a71a8b684d229c9e161f585532dca731832fef4
Viktor Fonic <viktor.fonic@gmail.com>
2017-06-01 20:07:08 +0800

    Client: Make client work with auth token

--- app/models/shopify_import/client.rb
+++ app/models/shopify_import/client.rb
@@ -5,11 +5,19 @@ module ShopifyImport
     include Singleton
     attr_reader :site
 
-    def get_connection(api_key: nil, password: nil, shop_name: nil)
+    def get_connection(api_key: nil, password: nil, shop_domain: nil, token: nil)
       @api_key = api_key || Spree::Config[:shopify_api_key]
       @password = password || Spree::Config[:shopify_password]
-      @shop_domain = shop_name || Spree::Config[:shopify_shop_domain]
-      @site = shopify_site
+      @shop_domain = shop_domain || Spree::Config[:shopify_shop_domain]
+      @token = token || Spree::Config[:shopify_token]
+
+      if password.present?
+        @site = shopify_site
+      elsif token.present?
+        ShopifyAPI::Session.new(@shop_domain, @token)
+      else
+        raise 'You must provide api_key together with password or authentication token'
+      end
     end
 
     private
